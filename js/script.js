0f9019946d18644688937e45eb7cd2ed28097d5e
System Administrator <root@eliecels-imac.home>
2021-01-03 11:04:57 -0500

    Finished highscore function

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 // Keeps track of total attemps
 let score = 20;
-
+let highScore = 0;
 //Allow to view the Secret Number on scree
-// document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = secretNumber;
 
 // Allows check button to function
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
   //Allows to restart the game
   document.querySelector('.again').addEventListener('click', function () {
     document.querySelector('.score').textContent = '20';
@@ -24,8 +25,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Start guessing...';
 
     document.querySelector('body').style.backgroundColor = '#222';
-
     document.querySelector('.guess').value = ' ';
+
+    document.querySelector('.highscore').textContent = highScore;
   });
 
   //Verify if no number was added to box
@@ -41,6 +43,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
 
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   // Results when answer is highier than the Secret Number
   else if (guess > secretNumber) {
